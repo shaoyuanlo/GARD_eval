0cf3571a172ae46e6332493599a2c206a3e0a217
Shao-Yuan Lo <42320029+shaoyuanlo@users.noreply.github.com>
2020-06-23 00:08:07 -0400

    Update model.py

--- video_classification/model.py
+++ video_classification/model.py
@@ -1,144 +1,22 @@
 import torch
 from torch import nn
 
-from models import resnet, pre_act_resnet, wide_resnet, wide_resnet_3bn, resnext, resnext_3bn, densenet
+from models import resnet, resnext_3bn
 
 
-def generate_model(opt):
-    assert opt.mode in ['score', 'feature']
-    if opt.mode == 'score':
-        last_fc = True
-    elif opt.mode == 'feature':
-        last_fc = False
+def generate_model(model_name):
 
-    assert opt.model_name in ['resnet', 'preresnet', 'wideresnet', 'wideresnet_3bn', 'resnext', 'resnext_3bn', 'densenet']
+    if model_name == 'resnet':
+        model = resnet.resnet50(num_classes=3, shortcut_type='B',
+                                sample_size=112, sample_duration=40,
+                                last_fc=True)
 
-    if opt.model_name == 'resnet':
-        assert opt.model_depth in [10, 18, 34, 50, 101, 152, 200]
+    elif model_name == 'resnext_3bn':
+        model = resnext_3bn.resnet101(num_classes=101, shortcut_type='B',
+            cardinality=32,
+            sample_size=112, sample_duration=40)
 
-        if opt.model_depth == 10:
-            model = resnet.resnet10(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                    sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                    last_fc=last_fc)
-        elif opt.model_depth == 18:
-            model = resnet.resnet18(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                    sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                    last_fc=last_fc)
-        elif opt.model_depth == 34:
-            model = resnet.resnet34(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                    sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                    last_fc=last_fc)
-        elif opt.model_depth == 50:
-            model = resnet.resnet50(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                    sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                    last_fc=last_fc)
-        elif opt.model_depth == 101:
-            model = resnet.resnet101(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                     sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                     last_fc=last_fc)
-        elif opt.model_depth == 152:
-            model = resnet.resnet152(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                     sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                     last_fc=last_fc)
-        elif opt.model_depth == 200:
-            model = resnet.resnet200(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                     sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                     last_fc=last_fc)
-    elif opt.model_name == 'wideresnet':
-        assert opt.model_depth in [50]
-
-        if opt.model_depth == 50:
-            model = wide_resnet.resnet50(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut, k=opt.wide_resnet_k,
-                                         sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                         last_fc=last_fc)
-    elif opt.model_name == 'resnext':
-        assert opt.model_depth in [50, 101, 152]
-
-        if opt.model_depth == 50:
-            model = resnext.resnet50(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut, cardinality=opt.resnext_cardinality,
-                                     sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                     last_fc=last_fc)
-        elif opt.model_depth == 101:
-            model = resnext.resnet101(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut, cardinality=opt.resnext_cardinality,
-                                      sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                      last_fc=last_fc)
-        elif opt.model_depth == 152:
-            model = resnext.resnet152(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut, cardinality=opt.resnext_cardinality,
-                                      sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                      last_fc=last_fc)
-
-
-    elif opt.model_name == 'resnext_3bn':
-        assert opt.model_depth in [101]		
-
-        model = resnext_3bn.resnet101(
-            num_classes=opt.n_classes,
-            shortcut_type=opt.resnet_shortcut,
-            cardinality=opt.resnext_cardinality,
-            sample_size=opt.sample_size,
-            sample_duration=opt.sample_duration)
-
-    elif opt.model_name == 'wideresnet_3bn':
-        assert opt.model_depth in [50]
-
-        if opt.model_depth == 50:
-            model = wide_resnet_3bn.resnet50(
-                num_classes=opt.n_classes,
-                shortcut_type=opt.resnet_shortcut,
-                k=opt.wide_resnet_k,
-                sample_size=opt.sample_size,
-                sample_duration=opt.sample_duration)			
-									  
-									  
-    elif opt.model_name == 'preresnet':
-        assert opt.model_depth in [18, 34, 50, 101, 152, 200]
-
-        if opt.model_depth == 18:
-            model = pre_act_resnet.resnet18(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                            sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                            last_fc=last_fc)
-        elif opt.model_depth == 34:
-            model = pre_act_resnet.resnet34(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                            sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                            last_fc=last_fc)
-        elif opt.model_depth == 50:
-            model = pre_act_resnet.resnet50(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                            sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                            last_fc=last_fc)
-        elif opt.model_depth == 101:
-            model = pre_act_resnet.resnet101(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                             sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                             last_fc=last_fc)
-        elif opt.model_depth == 152:
-            model = pre_act_resnet.resnet152(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                             sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                             last_fc=last_fc)
-        elif opt.model_depth == 200:
-            model = pre_act_resnet.resnet200(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut,
-                                             sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                             last_fc=last_fc)
-    elif opt.model_name == 'densenet':
-        assert opt.model_depth in [121, 169, 201, 264]
-
-        if opt.model_depth == 121:
-            model = densenet.densenet121(num_classes=opt.n_classes,
-                                         sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                         last_fc=last_fc)
-        elif opt.model_depth == 169:
-            model = densenet.densenet169(num_classes=opt.n_classes,
-                                         sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                         last_fc=last_fc)
-        elif opt.model_depth == 201:
-            model = densenet.densenet201(num_classes=opt.n_classes,
-                                         sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                         last_fc=last_fc)
-        elif opt.model_depth == 264:
-            model = densenet.densenet264(num_classes=opt.n_classes,
-                                         sample_size=opt.sample_size, sample_duration=opt.sample_duration,
-                                         last_fc=last_fc)
-
-    if not opt.no_cuda:
-        model = model.cuda()
-        model = nn.DataParallel(model, device_ids=None)
+    model = model.cuda()
+    model = nn.DataParallel(model, device_ids=None)
 
     return model
