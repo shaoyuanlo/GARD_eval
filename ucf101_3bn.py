7a1dc6dcb24897c3499eaf708d2b16051b2b83aa
Shao-Yuan Lo <42320029+shaoyuanlo@users.noreply.github.com>
2020-06-23 21:32:36 -0400

    Update ucf101_3bn.py

--- ucf101_3bn.py
+++ ucf101_3bn.py
@@ -255,7 +255,7 @@ def get_my_model(model_kwargs, wrapper_kwargs, weights_file):
 
     spatial_transform = Compose([Scale(112), CenterCrop(40), torchvision.transforms.ToTensor()])
 
-    wrapped_model = MyPytorchClassifier(detector, model, spatial_transform)
+    wrapped_model = MyPytorchClassifier(detector=detector, model=model, spatial_transform=spatial_transform)
 	
     return wrapped_model
 
