42117077290d8d04803abb1b2b41f30a99949409
Shao-Yuan Lo <42320029+shaoyuanlo@users.noreply.github.com>
2020-06-25 22:33:07 -0400

    Update ucf101_3bn.py

--- ucf101_3bn.py
+++ ucf101_3bn.py
@@ -234,7 +234,8 @@ class MyPytorchClassifier(PyTorchClassifier):
         # Compute the gradient and return
         model_outputs = detector_and_model(self.my_detector, self.my_model, inputs_t, self.spatial_transform)
 		
-        print(labels_t)		
+        print(labels_t)
+        print(model_outputs)		
 		
         loss = self._loss(model_outputs[-1], labels_t)
 
