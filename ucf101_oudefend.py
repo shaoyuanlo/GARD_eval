97eea8340f6d4af06ce3457b28b726d415d50629
Shao-Yuan Lo <42320029+shaoyuanlo@users.noreply.github.com>
2020-12-07 18:26:48 -0500

    Update ucf101_oudefend.py

--- ucf101_oudefend.py
+++ ucf101_oudefend.py
@@ -103,8 +103,7 @@ class MyPytorchClassifier(PyTorchClassifier):
         self.my_model.eval()		
 
         # Apply preprocessing
-        print(x.size)		
-        x_preprocessed = x
+        x_preprocessed = preprocessing_fn_torch(x)
 
         # Run prediction with batch processing
         results = np.zeros((x_preprocessed.shape[0], self.nb_classes), dtype=np.float32)
@@ -150,7 +149,7 @@ class MyPytorchClassifier(PyTorchClassifier):
             raise ValueError("Label %s is out of range." % label)
 
         # Apply preprocessing
-        x_preprocessed = x
+        x_preprocessed = preprocessing_fn_torch(x)
         x_preprocessed = torch.from_numpy(x_preprocessed).to(self._device)
 
         # Compute gradients
@@ -219,7 +218,7 @@ class MyPytorchClassifier(PyTorchClassifier):
         :return: Array of gradients of the same shape as `x`.
         """
         # Apply preprocessing
-        x_preprocessed = x
+        x_preprocessed = preprocessing_fn_torch(x)
         _, y_preprocessed = self._apply_preprocessing(x, y, fit=False)		
 
         # Check label shape
@@ -360,6 +359,100 @@ def preprocessing_fn(inputs):
         outputs.append(np.array(input_reshaped, dtype=np.float32))	
 		
     return outputs
+
+
+def preprocessing_fn_torch(
+    batch: Union[torch.Tensor, np.ndarray],
+    consecutive_frames: int = 40,
+    align_corners: bool = False,
+):
+    """
+    inputs - batch of videos each with shape (frames, height, width, channel)
+    outputs - batch of videos each with shape (n_stack, channel, stack_frames, new_height, new_width)
+        frames = n_stack * stack_frames (after padding)
+        new_height = new_width = 112
+    consecutive_frames - number of consecutive frames (stack_frames)
+    After resizing, a center crop is performed to make the image square
+    This is a differentiable alternative to MARS' PIL-based preprocessing.
+        There are some
+    """
+    if not isinstance(batch, torch.Tensor):
+        logger.warning(f"batch {type(batch)} is not a torch.Tensor. Casting")
+        batch = torch.from_numpy(batch).to(DEVICE)
+        # raise ValueError(f"batch {type(batch)} is not a torch.Tensor")
+    if batch.dtype != torch.float32:
+        raise ValueError(f"batch {batch.dtype} should be torch.float32")
+    if batch.shape[0] != 1:
+        raise ValueError(f"Batch size {batch.shape[0]} != 1")
+    video = batch[0]
+
+    if video.ndim != 4:
+        raise ValueError(
+            f"video dims {video.ndim} != 4 (frames, height, width, channel)"
+        )
+    if video.shape[0] < 1:
+        raise ValueError("video must have at least one frame")
+    if tuple(video.shape[1:]) == (240, 320, 3):
+        standard_shape = True
+    elif tuple(video.shape[1:]) == (226, 400, 3):
+        logger.warning("Expected odd example shape (226, 400, 3)")
+        standard_shape = False
+    else:
+        raise ValueError(f"frame shape {tuple(video.shape[1:])} not recognized")
+    if video.max() > 1.0 or video.min() < 0.0:
+        raise ValueError("input should be float32 in [0, 1] range")
+    if not isinstance(consecutive_frames, int):
+        raise ValueError(f"consecutive_frames {consecutive_frames} must be an int")
+    if consecutive_frames < 1:
+        raise ValueError(f"consecutive_frames {consecutive_frames} must be positive")
+
+    # Select a integer multiple of consecutive frames
+    while len(video) < consecutive_frames:
+        # cyclic pad if insufficient for a single stack
+        video = torch.cat([video, video[: consecutive_frames - len(video)]])
+    if len(video) % consecutive_frames != 0:
+        # cut trailing frames
+        video = video[: len(video) - (len(video) % consecutive_frames)]
+
+    # Attempts to directly follow MARS approach
+    # (frames, height, width, channel) to (frames, channel, height, width)
+    video = video.permute(0, 3, 1, 2)
+    if standard_shape:  # 240 x 320
+        sample_height, sample_width = 112, 149
+    else:  # 226 x 400
+        video = video[:, :, 1:-1, :]  # crop top/bottom pixels, reduce by 2
+        sample_height, sample_width = 112, 200
+
+    video = torch.nn.functional.interpolate(
+        video,
+        size=(sample_height, sample_width),
+        mode="bilinear",
+        align_corners=align_corners,
+    )
+
+    if standard_shape:
+        crop_left = 18  # round((149 - 112)/2.0)
+    else:
+        crop_left = 40
+    video = video[:, :, :, crop_left : crop_left + sample_height]
+
+    if video.max() > 1.0:
+        raise ValueError("Video exceeded max after interpolation")
+    if video.min() < 0.0:
+        raise ValueError("Video under min after interpolation")
+
+    # reshape into stacks of frames
+    video = torch.reshape(video, (-1, consecutive_frames) + video.shape[1:])
+
+    # transpose to (stacks, channel, stack_frames, height, width)
+    video = video.permute(0, 2, 1, 3, 4)
+    # video = torch.transpose(video, axes=(0, 4, 1, 2, 3))
+
+    # normalize before changing channel position?
+    video = torch.transpose(video, 1, 4)
+    video = torch.transpose(video, 4, 1)
+
+    return video	
 	
 	
 def get_my_model(model_kwargs, wrapper_kwargs, weights_file):
